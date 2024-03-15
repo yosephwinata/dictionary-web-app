2d715ab052551755e33a0cf992249a0a4a22206d
yosephwinata <yoseph.winata@gmail.com>
2024-03-15 22:36:11 +0700

    Add global colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black-1": "#050505",
+        "black-2": "#1F1F1F",
+        "black-3": "#2D2D2D",
+        "black-4": "#3A3A3A",
+        "gray-1": "#3A3A3A",
+        "gray-2": "#E9E9E9",
+        "gray-3": "#F4F4F4",
+        white: "#FFFFFF",
+        purple: "#A445ED",
+        orange: "#FF5252",
+      },
+    },
   },
   plugins: [],
 };

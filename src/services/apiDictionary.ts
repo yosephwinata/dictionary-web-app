5b42e1004ff3dad40ed82eb1ec961a3eaf404997
yosephwinata <yoseph.winata@gmail.com>
2024-03-18 17:00:11 +0700

    Add interface to axios

--- src/services/apiDictionary.ts
+++ src/services/apiDictionary.ts
@@ -1,10 +1,36 @@
 import axiosInstance from "../axios/axiosInstance";
 
-// interface DictionaryResponse {}
+interface Phonetic {
+  text: string;
+  audio: string;
+}
 
-export const getDictionary = async (keyword: string) => {
+interface Definition {
+  definition: string;
+  synonyms: string[];
+  antonyms: string[];
+  example?: string;
+}
+
+interface Meaning {
+  partOfSpeech: string;
+  definitions: Definition[];
+}
+
+interface DictionaryResponse {
+  word: string;
+  phonetics: Phonetic[];
+  meanings: Meaning[];
+  sourceUrls: string[];
+}
+
+export const getDictionary = async (
+  keyword: string,
+): Promise<DictionaryResponse[]> => {
   try {
-    const response = await axiosInstance.get(`/entries/en/${keyword}`);
+    const response = await axiosInstance.get<DictionaryResponse[]>(
+      `/entries/en/${keyword}`,
+    );
     // console.log("response", response); // Uncomment for debugging
     return response.data;
   } catch (error) {

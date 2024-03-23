c0f3f53d5bb49c60cc09fc63a26a51f8f46983b4
yosephwinata <yoseph.winata@gmail.com>
2024-03-23 15:31:38 +0700

    Add notes for future projects

--- src/hooks/useSyncDarkMode.ts
+++ src/hooks/useSyncDarkMode.ts
@@ -11,6 +11,17 @@ const useSyncDarkMode = () => {
       document.documentElement.classList.remove("dark");
     }
   }, [isDarkMode]);
+
+  // Use the code below for future projects where you want to implement 3 choices in a dropdown: light, dark, system:
+  // if (
+  //   theme === "dark" ||
+  //   (theme === "system" &&
+  //     window.matchMedia("(prefers-color-scheme: dark)").matches)
+  // ) {
+  //   document.documentElement.classList.add("dark");
+  // } else {
+  //   document.documentElement.classList.remove("dark");
+  // }
 };
 
 export default useSyncDarkMode;
